d400cea1d8d1280d1e0de8a57d07992131bae430
Bradford Bruce <nj_brad@outlook.com>
2022-12-17 08:58:26 -0500

    CountDown improvements

---
+++ LocalEdit/CountDownTypes/CountDownConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using LocalEdit.FlowTypes;
+
+namespace LocalEdit.CountDownTypes
+{
+    public class CountDownConfiguration
+    {
+        public TimeSpan? StartTime { get; set; } = DateTime.Now.TimeOfDay;
+        public TimeSpan? EndTime { get; set; } = DateTime.Now.TimeOfDay.Add(new TimeSpan(1, 0, 0));
+        public int WarningMinutes { get; set; } = 5;
+    }
+}
---
+++ LocalEdit/Modals/CountDownModal.razor.cs
@@ -0,0 +1,43 @@
+﻿using Blazorise;
+using LocalEdit.CountDownTypes;
+using Microsoft.AspNetCore.Components;
+
+namespace LocalEdit.Modals
+{
+    public partial class CountDownModal : LE_ModalBase
+    {
+        //private Modal? modalRef;
+        public CountDownConfiguration Configuration { get; set; } = new();
+
+        //Alert myAlert;
+
+        //public override Task Opened()
+        //{
+        //    if (ParentType == C4TypeEnum.Unknown)
+        //    {
+        //        myAlert.Show();
+        //    }
+
+        //    return Task.CompletedTask;
+        //}
+
+
+        Validations? validations;
+
+        public async Task<bool> IsValid()
+        {
+            bool rtnVal = false;
+            if (await validations.ValidateAll())
+            {
+                rtnVal = true;
+            }
+
+            return rtnVal;
+        }
+
+        public override async Task ResetValidation()
+        {
+            await validations.ClearAll();
+        }
+    }
+}
